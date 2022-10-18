519eac9ffe04f24e1f9fcda32766cef15dc954d2
slackstring <slackstring88@gmail.com>
2022-10-18 18:39:59 +0200

    initial build

---
+++ ÜberladungMethoden/ÜberladungMethoden/Dog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÜberladungMethoden
+{
+    public class Dog
+    {
+        //Eigenschaften
+        string Name { get; set; }
+        int Age { get; set; }
+        string Owner { get; set; }
+        //Konstruktor
+        public Dog(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            Owner = "No owner";
+        }
+        public Dog(string name, int age, string owner)
+        {
+            Name = name;
+            Age = age;
+            Owner = owner;
+        }
+
+        public void WriteData()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+            Console.WriteLine(Owner);
+        }
+    }
+}
---
+++ ÜberladungMethoden/ÜberladungMethoden/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÜberladungMethoden
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog Cooper = new Dog("Cooper", 8, "Fam. Dapper");
+            Dog Buba = new Dog("Buba", 8);
+            Cooper.WriteData();
+            Buba.WriteData();
+            Console.ReadKey();
+        }
+
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        public static double Sum(double[] numbers)
+        {
+            double result=0;
+            foreach (double d in numbers)
+            {
+                result += d;
+            }
+            return result;
+        }
+    }
+}
